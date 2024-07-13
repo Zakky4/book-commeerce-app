d686f03071f613e971564daaf0826cb8783686fb
zakky <sadaoza@gmail.com>
2024-07-13 10:54:50 +0900

    Add GitHub Login

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { NextAuthProvider } from "./lib/next-auth/provider";
+import { Suspense } from "react";
+// import Loading from "./loading";
 
 const notoSansJP = Noto_Sans_JP({ weight: "400", subsets: ["latin"] });
 
@@ -12,14 +15,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ja">
       <body className={notoSansJP.className}>
-        <Header />
-        {children}
+        <NextAuthProvider>
+          <Header />
+          {/* <Suspense fallback={<Loading />}> */}
+            {children}
+            {/* <CreateBookButton /> */}
+          {/* </Suspense> */}
+        </NextAuthProvider>
       </body>
     </html>
   );
